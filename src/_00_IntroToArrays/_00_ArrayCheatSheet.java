1254bf5e731c8e56b9a79ed295f5ba0ee62f74b3
SeahawksRawesome <league@WTS22.attlocal.net>
2018-07-22 17:25:46 -0700

    on 00

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -1,33 +1,39 @@
 package _00_IntroToArrays;
 
-import java.util.Random;
-
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
+		// 1. make an array of 5 Strings
+		String[] x = { "Hi", "Hello", "What's up?", "Cool", "*screams internally*" };
+		// 2. print the third element in the array
+		System.out.println(x[2]);
+		// 3. set the third element to a different value
+		x[3] = "Oeuf";
+		// 4. print the third element again
+		System.out.println(x[2]);
+		// 5. use a for loop to set all the elements in the array to a string of your
+		// choice
+		for (int i = 0; i < x.length; i++) {
+			x[i] = "WORD!";
+		}
+		// 6. use a for loop to print all the values in the array
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
+		for (int i = 0; i < x.length; i++) {
+			System.out.println(x[i]);
+		}
+		// 7. make an array of 50 integers
+		int[] y = new int[50];
+		// 8. use a for loop to make every value of the integer array a random number
+		for (int i = 0; i < y.length; i++) {
 
-		//2. print the third element in the array
+		}
+		// 9. without printing the entire array, print only the smallest number on the
+		// array
 
-		//3. set the third element to a different value
+		// 10 print the entire array to see if step 8 was correct
 
-		//4. print the third element again
-		
-		//5. use a for loop to set all the elements in the array to a string of your choice
-		
-		//6. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		//7. make an array of 50 integers
+		// 11. print the largest number in the array.
 
-		//8. use a for loop to make every value of the integer array a random number
+		// 12. print only the last element in the array
 
-		//9. without printing the entire array, print only the smallest number on the array
-
-		//10 print the entire array to see if step 8 was correct
-
-		//11. print the largest number in the array.
-		
-		//12. print only the last element in the array
-		
 	}
 }
